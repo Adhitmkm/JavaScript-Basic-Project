c2b4c101a08bfe66597b651be1d4af947d02ebb2
Adhitmkm <adhitmkm@gmail.com>
2025-02-06 00:16:14 +0530

    Rsponsive-Navigation-Bar completed

---
+++ Responsive-Navigation-Bar/script.js
@@ -0,0 +1,8 @@
+let toogleLogo = document.querySelector(".toogle-logo") 
+console
+let menu = document.querySelector('.menu')
+
+toogleLogo.addEventListener('click',function(){
+    // menu.classList.add or .remove  inghaney namuk class add akanum remove aakanum parttum  toogle aananakil undakil remove illankil active
+    menu.classList.toggle('is-active')
+})
